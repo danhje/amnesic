1cf3645a0e36fc7b1ff523769f74bbcb0d3defe9
Daniel Hjertholm <danhje@gmail.com>
2020-09-05 23:46:18 +0200

    TTL fully implemented and working

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # thymesis
 Memoization decorator for Python, with optional TTL (measured in time or function calls) for the cached results.
+
+<!--
+TODO:
+
+Installation
+- pypi
+
+Requires
+- python 3.8 or above
+
+Usage
+- Basic usage with function
+- With options
+- With method
+- Treats function aliases as identical to function
+- Method on different objects not treated as the same
+- Warning: Redefined function assumed to be the same
+
+How it works
+
+Build status
+-->
--- thymesis/thymesis.py
+++ thymesis/thymesis.py
@@ -5,19 +5,23 @@ Memoization for Python, with optional TTL (measured in time or function call cou
 
 from sys import modules
 from enum import Enum
+from time import time
 
 
 class TTLUnit(Enum):
+    SECONDS = 'seconds'
     MINUTES = 'minutes'
     CALL_COUNT = 'call_count'
 
 
 class Cache(dict):
-    def addData(self, hash, data, ttl=None, ttl_unit=TTLUnit.MINUTES):
+    def addData(self, hash, data, ttl=None, ttl_unit=TTLUnit.SECONDS):
         dataobj = {'data': data}
         if ttl is not None and ttl_unit is not None:
             dataobj['ttl'] = ttl
             dataobj['ttl_unit'] = ttl_unit
+        if ttl_unit in (TTLUnit.MINUTES, TTLUnit.SECONDS):
+            dataobj['timestamp'] = time()
         self[hash] = dataobj
 
     def getDataIfCached(self, hash):
@@ -27,13 +31,18 @@ class Cache(dict):
         data = dataobj['data']
         if 'ttl' not in dataobj or 'ttl_unit' not in dataobj:
             return data
-        if dataobj['ttl_unit'] == TTLUnit.CALL_COUNT:   
+        if dataobj['ttl_unit'] == TTLUnit.CALL_COUNT:
             dataobj['ttl'] -= 1
             if dataobj['ttl'] <= 0:
                 del self[hash]
             return data
-        elif dataobj['ttl_unit'] == TTLUnit.MINUTES:
-            return data
+        elif dataobj['ttl_unit'] in (TTLUnit.SECONDS, TTLUnit.MINUTES):
+            ttl_seconds = dataobj['ttl'] if dataobj['ttl_unit'] == TTLUnit.SECONDS else 60.0 * dataobj['ttl']
+            if time() - dataobj['timestamp'] < ttl_seconds:
+                return data
+            else:
+                del self[hash]
+                return None
         else:
             raise ValueError(f'Unknown ttl_unit: {dataobj["ttl_unit"]}')
 
@@ -47,13 +56,12 @@ def memoize(func=None, ttl=None, ttl_unit=None):
         def memoized_func(*args, **kwargs):
             invocation_string = '~'.join((
                 func.__name__,
-                str(id(func)),
                 '~'.join((str(arg) for arg in args)),
                 '~'.join((f'{str(k)}:{str(v)}' for k, v in kwargs.items()))
             ))
             invocation_hash = hash(invocation_string)
-            if (data := this._cache.getDataIfCached(invocation_hash)) is not None:
-                return data
+            if (cachedData := this._cache.getDataIfCached(invocation_hash)) is not None:
+                return cachedData
             function_result = func(*args, **kwargs)
             this._cache.addData(invocation_hash, function_result, ttl, ttl_unit)
             return function_result
@@ -63,13 +71,12 @@ def memoize(func=None, ttl=None, ttl_unit=None):
             def memoized_func(*args, **kwargs):
                 invocation_string = '~'.join((
                     func.__name__,
-                    str(id(func)),
                     '~'.join((str(arg) for arg in args)),
                     '~'.join((f'{str(k)}:{str(v)}' for k, v in kwargs.items()))
                 ))
                 invocation_hash = hash(invocation_string)
-                if (data := this._cache.getDataIfCached(invocation_hash)) is not None:
-                    return data
+                if (cachedData := this._cache.getDataIfCached(invocation_hash)) is not None:
+                    return cachedData
                 function_result = func(*args, **kwargs)
                 this._cache.addData(invocation_hash, function_result, ttl, ttl_unit)
                 return function_result
@@ -94,18 +101,37 @@ if __name__ == '__main__':
     assert(time() - start < 1)
 
     @memoize(ttl=1, ttl_unit=TTLUnit.CALL_COUNT)
-    def slowGreetingGenerator(fname, lname, *args, **kwargs):
+    def slowGreetingGenerator2(fname, lname, *args, **kwargs):
         sleep(1)
         return f'Hello, {fname} {lname}'
 
     start = time()
-    slowGreetingGenerator('Daniel', 'Hjertholm', 'Developer', age=34)
+    slowGreetingGenerator2('Daniel', 'Hjertholm', 'Developer', age=34)
     assert(time() - start >= 1)
 
     start = time()
-    slowGreetingGenerator('Daniel', 'Hjertholm', 'Developer', age=34)
+    slowGreetingGenerator2('Daniel', 'Hjertholm', 'Developer', age=34)
     assert(time() - start < 1)
 
     start = time()
-    slowGreetingGenerator('Daniel', 'Hjertholm', 'Developer', age=34)
+    slowGreetingGenerator2('Daniel', 'Hjertholm', 'Developer', age=34)
+    assert(time() - start >= 1)
+
+    @memoize(ttl=3, ttl_unit=TTLUnit.SECONDS)
+    def slowGreetingGenerator3(fname, lname, *args, **kwargs):
+        sleep(1)
+        return f'Hello, {fname} {lname}'
+
+    start = time()
+    slowGreetingGenerator3('Daniel', 'Hjertholm', 'Developer', age=34)
+    assert(time() - start >= 1)
+
+    start = time()
+    slowGreetingGenerator3('Daniel', 'Hjertholm', 'Developer', age=34)
+    assert(time() - start < 1)
+
+    sleep(3)
+
+    start = time()
+    slowGreetingGenerator3('Daniel', 'Hjertholm', 'Developer', age=34)
     assert(time() - start >= 1)
