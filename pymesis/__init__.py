362bf23c29770d1e2540134a2d4a4aa6c6201369
Daniel Hjertholm <danhje@gmail.com>
2020-09-06 23:09:59 +0200

    Bump version to 0.0.3

--- pymesis/__init__.py
+++ pymesis/__init__.py
@@ -4,7 +4,7 @@ Memoization for Python, with optional TTL (measured in time or function call cou
 
 
 __author__ = 'Daniel Hjertholm'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 
 from .pymesis import memoize, TTLUnit
