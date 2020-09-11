9411bd66503edf5102b0c76addff5e39a6127af0
Daniel Hjertholm <danhje@gmail.com>
2020-09-11 08:04:28 +0200

    More testing

--- tests/test_pymesis.py
+++ tests/test_pymesis.py
@@ -1,2 +1,15 @@
+from pymesis import Cache, memoize
+
+
 def test_pytest():
     assert True
+
+
+def test_cache():
+    cache = Cache()
+
+    original_data = 'data'
+    cache.add_data(hash('key'), original_data)
+    retrieved_data = cache.get_data_if_cached(hash('key'))
+    assert retrieved_data == original_data
+
