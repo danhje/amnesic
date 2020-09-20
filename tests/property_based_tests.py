b7aa9bb42a550a4da9ae102a154655cfb881bfeb
Daniel Hjertholm <danhje@gmail.com>
2020-09-20 20:18:38 +0200

    Experimenting with property-based testing

---
+++ tests/property_based_tests.py
@@ -0,0 +1,33 @@
+from pymesis import memoize, TTLUnit
+from pymesis import _cache as pymesis_cache
+from hypothesis import given, settings, assume
+import hypothesis.strategies as st
+
+
+strategies = (
+    st.integers(),
+    st.text(),
+    st.binary(),
+    st.booleans(),
+    st.characters(),
+    st.complex_numbers(allow_nan=False),
+    st.dates(),
+    st.datetimes(),
+    st.decimals(allow_nan=False),
+    st.dictionaries(st.text(), st.text()),
+    st.floats(allow_nan=False),
+    st.fractions(),
+    st.functions(),
+    st.iterables(st.text()),
+    st.none()
+)
+
+
+@given(st.one_of(strategies))
+def test_cache(data):
+    pymesis_cache.clear_cache()
+
+    pymesis_cache.add_data(hash('key'), data)
+    retrieved_data = pymesis_cache.get_data_if_cached(hash('key'))
+
+    assert retrieved_data == data
